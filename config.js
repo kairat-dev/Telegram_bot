897e63d734d694d4518e99083a023c64a176d8e9
Kairat <aptrapov.kairat@gmail.com>
2024-01-28 04:09:28 +0600

    first

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    "telegramToken": "6462568621:AAGL_TwHmemqps0qBilcPFiHc2_IBuj8LFU",
+    "catUrl": "https://api.thecatapi.com/v1/images/search",
+    "weatherUrl": "http://api.weatherapi.com/v1/current.json?key=ec3303d61b1348d09b7144500242601&q=",
+}
